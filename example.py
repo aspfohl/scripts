773cd2e939df38baedf14ae6a1e661f6aa24080b
Anna Pfohl <anna@mosaicml.com>
2023-04-11 20:27:11 -0700

    bump

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
+import argparse
+import os
+import time
 
-import os, argparse, time
 from mcli.sdk import update_run_metadata
 
 parser = argparse.ArgumentParser()
@@ -7,7 +9,11 @@ parser.add_argument('--n', type=int, required=True)
 args = parser.parse_args()
 
 run_name = os.environ.get('RUN_NAME')
-metadata = {"step": args.n, "wallclock/remaining_estimate": f"{30 - args.n} seconds"}
+
+metadata = {
+    "step": args.n,
+    "wallclock/remaining_estimate": f"{60 - args.n} seconds",
+}
 update_run_metadata(run_name, metadata)
 print(f"{args.n}: Updated metadata for {run_name} {metadata}")
 
